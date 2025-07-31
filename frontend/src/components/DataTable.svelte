4f79867f54d0f63116e14533f134d19dc3ef8f6b
Talha Khan <talha7k@gmail.com>
2025-07-31 14:53:53 +0300

    refactor(DataTable): replace custom svg icons with font awesome icons
    Simplify icon implementation by using Font Awesome classes instead of custom SVG markup

--- frontend/src/components/DataTable.svelte
+++ frontend/src/components/DataTable.svelte
@@ -108,9 +108,7 @@
       {#if showSearch}
         <div class="search-container">
           <div class="search-icon">
-            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-            </svg>
+            <i class="fas fa-search"></i>
           </div>
           <FormField
             type="text"
@@ -129,9 +127,7 @@
   <div class="table-content">
     {#if loading}
       <div class="loading-state">
-        <svg class="w-5 h-5 animate-spin" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path>
-        </svg>
+        <i class="fas fa-spinner fa-spin"></i>
         <span>Loading...</span>
       </div>
     {:else if data.length === 0}
